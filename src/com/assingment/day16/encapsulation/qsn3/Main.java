6452ee95e60df0382da76d328b65eb29bb318696
iraladinesh <iraladinesh@gmail.com>
2025-09-24 23:12:10 -0400

    Day 16 Encapsulation

---
+++ src/com/assingment/day16/encapsulation/qsn3/Main.java
@@ -0,0 +1,18 @@
+package com.assingment.day16.encapsulation.qsn3;
+
+
+public class Main {
+ public static void main(String[] args) {
+     Rectangle rectangle = new Rectangle();
+
+     rectangle.setLength(6.7);
+     rectangle.setWidth(12.0);
+
+     double length = rectangle.getLength();
+     double width = rectangle.getWidth();
+
+     System.out.println("Length: " + length);
+     System.out.println("Width: " + width);
+ }
+}
+
---
+++ src/com/assingment/day16/encapsulation/qsn7/Main.java
@@ -0,0 +1,25 @@
+package com.assingment.day16.encapsulation.qsn7;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+ public static void main(String[] args) {
+     Student student = new Student();
+
+     student.setStudent_id(1);
+     student.setStudent_name("Nadia Hyakinthos");
+
+     student.addGrade(92.5);
+     student.addGrade(89.0);
+     student.addGrade(90.3);
+
+     int student_id = student.getStudent_id();
+     String student_name = student.getStudent_name();
+     List<Double> grades = student.getGrades();
+
+     System.out.println("Student ID: " + student_id);
+     System.out.println("Student Name: " + student_name);
+     System.out.println("Grades: " + grades);
+ }
+}
+
