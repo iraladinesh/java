873fb128070decb4ddcadd97e748b0653e725487
iraladinesh <iraladinesh@gmail.com>
2025-10-07 23:55:16 -0400

    Day 22 - HashMap

---
+++ src/com/assignment/map/day22/Program11.java
@@ -0,0 +1,23 @@
+package com.assignment.map.day22;
+
+import java.util.*;
+public class Program11 {
+	
+	public static void main(String args[]){  
+		 
+		  HashMap<Integer,String> hash_map= new HashMap<Integer,String>();  
+		  
+		  hash_map.put(1,"Red");
+		  hash_map.put(2,"Green");
+		  hash_map.put(3,"Black");
+		  hash_map.put(4,"White");
+		  hash_map.put(5,"Blue");
+		  
+		  // get keyset value from map
+		   Set keyset = hash_map.keySet();
+		      
+		  // check key set values
+		   System.out.println("Key set values are: " + keyset);  
+		 }
+
+}
